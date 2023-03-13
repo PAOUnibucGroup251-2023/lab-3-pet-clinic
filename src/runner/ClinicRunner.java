b76106dad1c877f7be4e8c04518639df276575ec
Mihai-Tudor Popescu <MihaiTudor.Popescu@endava.com>
2023-03-13 16:12:53 +0200

    Refactoring & fixes

--- src/runner/ClinicRunner.java
+++ src/runner/ClinicRunner.java
@@ -9,41 +9,49 @@ import java.util.Scanner;
 
 public class ClinicRunner {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("tratament.txt"));
-        Treatment[] treatments = null;
-        while (input.hasNextLine()){
-            String line = input.nextLine();
-            String[] fields = line.split(",");
-            Treatment t = new Treatment(fields[0], Double.parseDouble(fields[1]));
-            if (t != null) {
-                if (treatments == null) {
-                    treatments = new Treatment[1];
-                } else {
-                    treatments = Arrays.copyOf(treatments, treatments.length + 1);
-                }
-                treatments[treatments.length - 1] = t;
-            }
-        }
-        input.close();
+        searchPatientAndDisplayDetails(readAnimals());
+    }
 
+    private static void searchPatientAndDisplayDetails(Animal[] animals) {
+        lookUpPatient(animals, readLookUpPatientName());
+    }
+
+    private static String readLookUpPatientName() {
+        Scanner screenInput = new Scanner(System.in);
+        System.out.println("Introduceti un nume: ");
+        String lookUpName = screenInput.nextLine();
         System.out.println();
+        return lookUpName;
+    }
 
+    private static void lookUpPatient(Animal[] animals, String lookUpName) {
+        boolean found = false;
+        if (animals != null && animals.length != 0) {
+            for (Animal a : animals) {
+                if (lookUpName != null && lookUpName.equals(a.getName())) {
+                    found = true;
+                    a.displayAllDetailsToConsole();
+                }
+            }
+        } else {
+            System.out.println("Lista de pacienti este goala.");
+        }
 
+        if (!found){
+            System.out.println("Animalul cautat nu a fost gasit.");
+        }
+    }
+
+    private static Animal[] readAnimals() throws FileNotFoundException {
+        Treatment[] treatments = readTreatmentsFromFile();
+        Scanner input;
         input = new Scanner(new File("pacienti.txt"));
         Animal[] animals = null;
         while (input.hasNextLine()){
             String line = input.nextLine();
             String[] mapping = line.split("-");
-            String[] animal = mapping[0].split(",");
-            String[] treatmentNameArray = mapping[1].split(",");
-            Animal a = null;
 
-            switch(animal[0]){
-                case "pisica": a = new Cat(animal[1],animal[2],animal[3],Integer.parseInt(animal[4])); break;
-                case "caine": a = new Dog(animal[1], Double.parseDouble(animal[2]),animal[3],Integer.parseInt(animal[4]));break;
-                case "papagal": a = new Parrot(animal[1], animal[2], Boolean.parseBoolean(animal[3])); break;
-                default: System.out.println("Intrare nevalida detectata " + line);
-            }
+            Animal a = createAnimal(line, mapping);
 
             if (a != null) {
                 String[] treatmentDescriptions = mapping[1].split(",");
@@ -63,26 +71,38 @@ public class ClinicRunner {
                 animals[animals.length - 1] = a;
             }
         }
+        return animals;
+    }
 
-        Scanner screenInput = new Scanner(System.in);
-        System.out.println("Introduceti un nume: ");
-        String lookUpName = screenInput.nextLine();
-        System.out.println();
+    private static Animal createAnimal(String line, String[] mapping) {
+        Animal a = null;
+        String[] animal = mapping[0].split(",");
+        switch(animal[0]){
+            case "pisica": a = new Cat(animal[1],animal[2],animal[3],Integer.parseInt(animal[4])); break;
+            case "caine": a = new Dog(animal[1], Double.parseDouble(animal[2]),animal[3],Integer.parseInt(animal[4]));break;
+            case "papagal": a = new Parrot(animal[1], animal[2], Boolean.parseBoolean(animal[3])); break;
+            default: System.out.println("Intrare nevalida detectata " + line);
+        }
+        return a;
+    }
 
-        boolean found = false;
-        if (animals != null && animals.length != 0) {
-            for (Animal a : animals) {
-                if (lookUpName != null && lookUpName.equals(a.getName())) {
-                    found = true;
-                    a.displayAllDetailsToConsole();
+    private static Treatment[] readTreatmentsFromFile() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("tratament.txt"));
+        Treatment[] treatments = null;
+        while (input.hasNextLine()){
+            String line = input.nextLine();
+            String[] fields = line.split(",");
+            Treatment t = new Treatment(fields[0], Double.parseDouble(fields[1]));
+            if (t != null) {
+                if (treatments == null) {
+                    treatments = new Treatment[1];
+                } else {
+                    treatments = Arrays.copyOf(treatments, treatments.length + 1);
                 }
+                treatments[treatments.length - 1] = t;
             }
-        } else {
-            System.out.println("Lista de pacienti este goala.");
         }
-
-        if (!found){
-            System.out.println("Animalul cautat nu a fost gasit.");
-        }
+        input.close();
+        return treatments;
     }
 }
